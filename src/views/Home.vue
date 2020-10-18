fa791bfba068856df741d3c72e25f98ae1437434
Ryo Sasaki <wor.ryosasaki@gmail.com>
2020-10-19 08:53:59 +0900

    要件③完了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="h-screen bg-gray-100">
         <HeaderLogout />
+        <div>
+            <p class="pt-10 pb-3 text-center text-2xl">{{ name }}さん、ようこそ</p>
+            <p class="text-right pr-10 text-xl">残高：{{ money }}円</p>
+        </div>
     </div>
 </template>
 
@@ -9,6 +13,27 @@ import HeaderLogout from '@/components/HeaderLogout.vue';
 export default {
     components: {
         HeaderLogout,
+    },
+    computed: {
+        name: {
+            get() {
+                return this.$store.state.name;
+            },
+            set(value) {
+                this.$store.commit('name', value);
+            },
+        },
+        money: {
+            get() {
+                return this.$store.state.money;
+            },
+            set(value) {
+                this.$store.commit('money', value);
+            },
+        },
+    },
+    created() {
+        this.$store.dispatch('createDashBoard');
     },
 };
 </script>
